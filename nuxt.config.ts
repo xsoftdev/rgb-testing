b3067ad70eb736edbddf5b88645964542f9ed4c8
xsoftdev <justanimee.dev@gmail.com>
2024-05-21 03:45:25 +0200

    Nuxt config / .env corrected

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,8 +7,6 @@ export default defineNuxtConfig({
     privateRuntimeConfig: {
       mongodb_url: process.env.MONGO_URI,
       secret_key: process.env.SECRET_KEY,
-      items_collection: process.env.ITEMS_COLLECTION,
-      db_name: process.env.DB_NAME,
       cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
       cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
       cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET
